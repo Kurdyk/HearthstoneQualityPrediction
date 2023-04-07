7fe89a9fad1f2c4a8173c8631c14b0b1c3408408
Marwan TRG <marwan@MacBook-Pro-de-Marwan.local>
2023-04-07 17:17:29 +0200

    add dataEncoding and implementation of TF-IDF

---
+++ src/dataEncoding.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import re
+
+class DataEncoder:
+	def __init__(self,df: pd.DataFrame):
+		self.df = df
+
+	def tokenization(self, documents):
+		"""
+		Use TF-IDF to encode the text and modifie the dataframe accordingly
+		"""
+		documents = self.df[column]
+
+		# Compute TF and IDF
+		tf_array = list() # array of dict, each dict represent a document and contain the tf of each words in the document
+		idf_dict = dict() # dict of words, each words is associated with the number of document which contain this word
+
+		for document in documents:
+			tf = dict()
+
+			delimited_document = re.split(' | , | ; | :',document)
+
+			for word in delimited_document:
+				# idf update
+				if word in idf_dict:
+					idf_dict[word] += 1
+				else:
+					idf_dict[word] = 0
+
+				# tf update
+				if word in tf:
+					tf[word] += 1
+				else:
+					tf[word] = 0
+
+			for word in tf:
+				tf[word] /= len(tf)
+
+
+			tf_array.append(tf)
+
+			
+
+
+if __name__ == "__main__":
+	df = pd.read_csv("../hearthstone.csv")
+	de = DataEncoder(df)
+	de.tokenization("name")
--- src/dataExploration.py
+++ src/dataExploration.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 
 class DataVisualizer:
@@ -12,21 +11,17 @@ class DataVisualizer:
     def filter_isin(self, varible, wanted_set):
         self.df = self.df[self.df[varible].isin(wanted_set)]
 
+    def print_col(self,columns):
+        print(self.df[columns])
+
     def print(self):
-        print(self.df)
+        print(self.df[columns])
 
 
 if __name__ == "__main__":
     df = pd.read_csv("../hearthstone.csv")
     dv = DataVisualizer(df)
-    print(df.keys())
     unwanted_col = ["cardId", "dbfId", "locale", "elite", "img", "flavor", "artist", "imgGold", "howToGetGold",
-                    "howToGet", "howToGetDiamond", "faction", "classes"]
+                    "howToGet", "howToGetDiamond", "faction"]
     dv.filter_col(unwanted_col)
-    dv.filter_isin("Category", set(df["Category"].unique()) - {"Missions", "Credits", "Hero Skins", "Tavern Brawl",
-                                                               "Battlegrounds", "Mercenaries"})
-    dv.filter_isin("type", set(df["type"].unique()) - {"Hero Power", "Enchantment", np.nan, "Hero"})
-    dv.filter_isin("collectible", set(df["collectible"].unique()) - {np.nan})
-    dv.filter_col(["collectible"])
-    dv.print()
-    dv.df.to_csv("../filterData.csv")
+ 
