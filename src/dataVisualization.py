f686ee9f681a409dacb0c82c1aca4ff11bd018f7
MarwanTRG <67915845+Azerty-ctrl-dot@users.noreply.github.com>
2023-05-02 12:22:32 +0200

    Update dataVisualization.py

--- src/dataVisualization.py
+++ src/dataVisualization.py
@@ -10,19 +10,19 @@ def plot_data(df,n_dim):
 	pca = PCA(n_components=n_dim)
 	X_pca = pca.fit_transform(df)
 
-    if n_dim == 1:
-        plt.plot(X_pca)
-        plt.show()
-    elif n_dim == 2:
-        plt.scatter(X_pca[:,0], X_pca[:,1])
-        plt.show()
-    elif n_dim == 3:
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(X_pca[:,0], X_pca[:,1], X_pca[:,2])
-        plt.show()
-    else:
-        print("Dimension not supported for plotting")
+	if n_dim == 1:
+		plt.plot(X_pca)
+		plt.show()
+	elif n_dim == 2:
+		plt.scatter(X_pca[:,0], X_pca[:,1])
+		plt.show()
+	elif n_dim == 3:
+		fig = plt.figure()
+		ax = fig.add_subplot(111, projection='3d')
+		ax.scatter(X_pca[:,0], X_pca[:,1], X_pca[:,2])
+		plt.show()
+	else:
+		print("Dimension not supported for plotting")
 
 if __name__ == "__main__":
 	df = pd.read_csv("../hearthstone.csv")
