d5e8c9ee413e53cbde38f5d345eb0269d3185e23
Kurdyk <louis.kurdyk@etu.u-paris.fr>
2023-05-06 11:27:44 +0200

    Grades visualization

--- src/dataVisualization.py
+++ src/dataVisualization.py
@@ -4,6 +4,7 @@ from sklearn.decomposition import PCA
 from dataEncoder import *
 import pandas as pd
 
+
 def plot_data(df, n_dim):
 	"""
 	Warning : df must not contain the labels.
@@ -26,8 +27,22 @@ def plot_data(df, n_dim):
 		print("Dimension not supported for plotting")
 
 
+def plot_grades(grade_df):
+	n, bins, patches = plt.hist(grade_df, bins=10, density=False, cumulative=False)
+
+	plt.title('Repartition of grades')
+	plt.xlabel('Grades')
+	plt.ylabel('Number of card')
+
+	plt.show()
+
+
 if __name__ == "__main__":
-	df = pd.read_csv("../HSTopdeck.csv").drop(columns=["card_type", "card_mark"]).set_index("name")
+	# Plotting label distribution
+	df = pd.read_csv("../HSTopdeck.csv")["card_mark"]
+	plot_grades(df)
+	# Plotting points in space
+	df = pd.read_csv("../HSTopdeck.csv").drop(columns=["card_type", "durability", "card_mark"]).set_index("name")
 	de = DataEncoder()
 	resulting_df = de.encode(df, 55).fillna(0)
 	n_dim = 3
