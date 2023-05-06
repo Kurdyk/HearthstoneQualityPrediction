18b660d42a409888febff3ed48b5fb0a9d07ae98
Marwan TRG <marwan@macbook-pro-de-marwan.home>
2023-05-06 17:08:05 +0200

    Changement normalisation -> scaling

--- src/hyperparameters_computation.py
+++ src/hyperparameters_computation.py
@@ -11,6 +11,7 @@ df['card_text'] = df['card_text'].str.replace('"', '')
 df['card_text'] = df['card_text'].str.replace(',', ' ')
 df['card_text'] = df['card_text'].str.replace(':', ' : ')
 df['card_text'] = df['card_text'].str.replace('.', ' ')
+df.to_csv("clean_df.csv",index=False)
 
 # Séparation train/test
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.75, shuffle=True)
@@ -23,14 +24,22 @@ x_train_encoded = data_encoder.encode(x_train, lsa_dim).fillna(0)
 x_test_encoded = data_encoder.encode(x_test, lsa_dim).fillna(0)
 
 # Normalisation des données / Scaling des données
+"""
 x_train_normalized = normalize(x_train_encoded)
 x_test_normalized = normalize(x_test_encoded)
+"""
+
+scaler = MinMaxScaler()
+cols_to_scale = ['mana', 'attack', 'health']
+x_train_encoded[cols_to_scale] = scaler.fit_transform(x_train_encoded[cols_to_scale])
+x_test_encoded[cols_to_scale] = scaler.fit_transform(x_test_encoded[cols_to_scale])
+
 
 # Visualisation des données
-plot_data(x_train_encoded,3)
+plot_data(x_train_normalized,3)
 
 # Création et entraînement du modèle
-lin_regressor = LinRegression(x_train_normalized, y_train, x_test_normalized, y_test)
+lin_regressor = LinRegression(x_train_encoded, y_train, x_test_encoded, y_test)
 lin_regressor.fit()
 
 # Évaluation du modèle
